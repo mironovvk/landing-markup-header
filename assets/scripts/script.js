eaafb1aacc6eff51dde4d2eaad3e83a04d89ca25
Кирилл Миронов <mironovk@plllc.ru>
2025-07-01 10:49:59 +0500

    code cleanup and comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,11 @@
 const topbar = document.getElementById('topbar');
 const header = document.getElementById('header');
+
+
+// Определяем высоту верхней панели (topbar) для дальнейшего использования
 const topbarHeight = topbar.offsetHeight;
 
+// Функция, которая задаёт верхний отступ для шапки (header) равным высоте topbar
 function setHeaderOffset() {
     let topbarHeight = topbar.offsetHeight;
     header.style.top = `${topbarHeight}px`;
@@ -9,12 +13,14 @@ function setHeaderOffset() {
     console.log(typeof(topbarHeight));
 }
 
+// Вызываем setHeaderOffset при загрузке страницы и при изменении размера окна
 window.addEventListener('load', setHeaderOffset);
 window.addEventListener('resize', setHeaderOffset);
 
 const darkLogo = document.querySelector('.logo--dark');
 const lightLogo = document.querySelector('.logo--light');
 
+// Отслеживаем прокрутку страницы
 window.addEventListener('scroll', () => {
     if (window.scrollY > topbarHeight) {
         header.classList.remove('header_transparent');
@@ -27,6 +33,8 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+// Обработчик клика по header
 header.addEventListener('click', () => {
     if (window.scrollY <= topbarHeight && header.classList.contains('header_transparent')) {
         header.classList.remove('header_transparent');
@@ -35,6 +43,7 @@ header.addEventListener('click', () => {
     }
 });
 
+// Обработчик потери фокуса у header
 header.addEventListener('blur', () => {
     if (window.scrollY <= topbarHeight && !header.classList.contains('header_transparent')) {
         header.classList.add('header_transparent');
@@ -44,6 +53,7 @@ header.addEventListener('blur', () => {
 const burgerBtn = document.getElementById('burger-btn');
 const navCollapse = document.getElementById('collapseExample');
 
+// Обработчик клика по кнопке бургера
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('active');
     navCollapse.classList.toggle('open');
@@ -54,6 +64,7 @@ burgerBtn.addEventListener('click', () => {
     }
 });
 
+// Для всех ссылок меню навигации добавляем обработчик клика
 document.querySelectorAll('.navbar-nav a').forEach(link => {
     link.addEventListener('click', () => {
         burgerBtn.classList.remove('active');
